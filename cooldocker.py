3f7bd8a962a0ee5596d8e7878bfdc9cf556998e5
lukasjoc <jochamlu@gmail.com>
2021-01-16 12:38:24 +0100

    fix time and port issue and clean up code

--- cooldocker.py
+++ cooldocker.py
@@ -1,51 +1,31 @@
 import docker
+from datetime import timedelta, datetime
 from tabulate import tabulate
-from datetime import datetime
 
 def container_info(client):
     container_data = []
-    container_cols = [ "CONTAINER ID", "IMAGE", "CREATED", "STATUS", "PORTS", "NAMES", "IP ADDRESS" ]
     for container in client.containers.list():
         attrs = container.attrs
-
-        def get_container():
-            return attrs["Config"]["Hostname"]
-
-        def get_container_image():
-            return attrs["Config"]["Image"]
-
-        # Fuck Timezones
         def get_container_created():
-            today_time = datetime.strptime(datetime.strftime(datetime.today(), '%Y-%m-%dT%H:%M:%S'), "%Y-%m-%dT%H:%M:%S")
-            create_time = datetime.strptime(attrs["Created"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
+            pattern = '%Y-%m-%dT%H:%M:%S'
+            today_time = datetime.strptime(datetime.strftime(datetime.today(), pattern), pattern)
+            create_time = datetime.strptime(attrs["Created"].split(".")[0], pattern)
             created = abs(today_time - create_time)
-            # if created.days >= 1:
-            #     return f"{created.days} days ago"
-            return created
+            if created.days >= 1:
+                return f"{created.days} days ago"
+            return str(timedelta(seconds=int(created.total_seconds())-3600))
 
-        # TODO: Calculate status time if up
-        # from ["State"]["StartedAt"], ["State"]["FinishedAt"]
-        def get_container_status():
-            return attrs["State"]["Status"]
-
-        # TODO: fix port to be HOSTIP:HOSTPORT->EXPOSED
         def get_container_ports():
             ports = attrs["NetworkSettings"]["Ports"]
-            hosted = []
-            for mappings in ports.values():
-                if mappings != None:
-                    for mapping in mappings:
-                        hosted.append(f"{mapping['HostIp']}:{mapping['HostPort']}")
-
             formatted_ports = ""
-            for hos in hosted:
-                formatted_ports += f"{hos}\xa0"
-            for exp in ports.keys():
-                formatted_ports += f"{exp}\xa0"
+            hosted = []
+            for port, mapping in ports.items():
+                if mapping != None:
+                    for item in mapping:
+                        formatted_ports += f"{item['HostIp']}:{item['HostPort']}->{port}\xa0"
+                else:
+                    formatted_ports += f"{port}\xa0"
             return formatted_ports
-
-        def get_container_name():
-            return attrs["Name"][1:]
 
         def get_container_ip():
             net_id = attrs["NetworkSettings"]["IPAddress"]
@@ -56,20 +36,20 @@ def container_info(client):
                 return attrs["NetworkSettings"]["Networks"][network_name]["IPAddress"]
 
         container_data.append([
-            get_container(),
-            get_container_image(),
+            attrs["Config"]["Hostname"],
+            attrs["Config"]["Image"],
             get_container_created(),
-            get_container_status(),
+            attrs["State"]["Status"],
             get_container_ports(),
-            get_container_name(),
+            attrs["Name"][1:],
             get_container_ip()
         ])
 
+    container_cols = [ "CONTAINER ID", "IMAGE", "CREATED", "STATUS", "PORTS", "NAMES", "IP ADDRESS" ]
     return container_data, container_cols
 
 if __name__ == "__main__":
-    cl = docker.from_env(max_pool_size=10)
-
+    cl = docker.from_env(max_pool_size=15)
     data, cols = container_info(client=cl)
     print(tabulate(data, headers=cols))
 
